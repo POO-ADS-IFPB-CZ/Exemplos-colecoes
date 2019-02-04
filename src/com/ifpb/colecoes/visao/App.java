4effbff14e965cc8db60bc64e1de5f7bf8991fdc
paulo <paulo.freitas.nt@gmail.com>
2019-02-04 07:48:57 -0300

    Usando fila de prioridades

--- src/com/ifpb/colecoes/visao/App.java
+++ src/com/ifpb/colecoes/visao/App.java
@@ -4,25 +4,27 @@ import com.ifpb.colecoes.modelo.Pessoa;
 
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class App {
 
     public static void main(String[] args) {
 
-        Deque<Pessoa> pessoas = new LinkedList<>();
+        Queue<Pessoa> pessoas = new PriorityQueue<>();
 
-        pessoas.offer(new Pessoa("222.222.222-02","João",20));
-        pessoas.offer(new Pessoa("111.111.111-01", "Maria", 21));
-        pessoas.offerFirst(new Pessoa("333.333.333-03", "Pedro", 30));
-        pessoas.offerLast(new Pessoa("444.444.444-04", "Ana", 25));
+        pessoas.offer(new Pessoa("111.111.111-01", "João", 20));
+        pessoas.offer(new Pessoa("222.222.222-02", "Maria", 19));
+        pessoas.offer(new Pessoa("333.333.333-03", "Pedro", 20));
+        pessoas.offer(new Pessoa("444.444.444-04","Ana", 18));
+        pessoas.offer(new Pessoa("555.555.555-05", "Joaquim", 20));
 
-        System.out.println("Próximo: "+pessoas.peekFirst());
-        System.out.println("Último: "+pessoas.peekLast());
+        //Não aceita elementos nulos
+        //pessoas.offer(null);
 
-        System.out.println(pessoas.poll());
-        System.out.println(pessoas.pollLast());
-        System.out.println(pessoas.pollFirst());
+        while(!pessoas.isEmpty()){
+            System.out.println(pessoas.poll());
+        }
 
     }
 
