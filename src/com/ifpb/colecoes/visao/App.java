7a666f35360a5f187a918a05ce61c297b3f784ea
paulo <paulo.freitas.nt@gmail.com>
2019-02-04 07:31:26 -0300

    Usando filas

--- src/com/ifpb/colecoes/visao/App.java
+++ src/com/ifpb/colecoes/visao/App.java
@@ -1,26 +1,25 @@
 package com.ifpb.colecoes.visao;
 
-import com.ifpb.colecoes.controle.CadastroPessoa;
 import com.ifpb.colecoes.modelo.Pessoa;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class App {
 
     public static void main(String[] args) {
 
+        Queue<Pessoa> pessoas = new LinkedList<>();
 
-        Set<Pessoa> pessoas = new TreeSet<>(Comparator.comparing(Pessoa::getCpf));
+        pessoas.offer(new Pessoa("222.222.222-02","João",20));
+        pessoas.offer(new Pessoa("111.111.111-01", "Maria", 21));
+        pessoas.offer(new Pessoa("333.333.333-03", "Pedro", 30));
 
-        pessoas.add(new Pessoa("111.111.111-01", "João",20));
-        pessoas.add(new Pessoa("111.111.111-01", "Maria",19));
-        pessoas.add(new Pessoa("222.222.222-02", "Pedro",17));
-        pessoas.add(new Pessoa("333.333.333-03", "Ana",20));
+        System.out.println("Próximo: "+pessoas.peek());
 
-        for(Pessoa p : pessoas){
-            System.out.println(p);
+        while (!pessoas.isEmpty()){
+            System.out.println(pessoas.poll());
         }
-
 
     }
 
